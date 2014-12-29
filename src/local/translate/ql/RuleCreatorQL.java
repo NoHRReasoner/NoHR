b42824f7f3d28525327354992c22ac0d05e23b5a
nunocosta <91nunocosta@gmail.com>
2014-12-29 21:51:51 +0000

    DL-LiteR translation auxiliary methods integration.

--- src/local/translate/ql/RuleCreatorQL.java
+++ src/local/translate/ql/RuleCreatorQL.java
@@ -9,13 +9,10 @@ import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLLiteral;
-import org.semanticweb.owlapi.model.OWLNaryPropertyAxiom;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLProperty;
 import org.semanticweb.owlapi.model.OWLPropertyExpression;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.model.OWLSubPropertyAxiom;
 
 public class RuleCreatorQL extends RuleCreator {
 
@@ -50,18 +47,18 @@ public class RuleCreatorQL extends RuleCreator {
 		//TODO implement
 	}
 	
-	private void s1(OWLSubClassOfAxiom axiom) {
+	private void s1(OWLClassExpression subclass, OWLClassExpression superclass) {
 		//TODO implement
 	}
 	
-    private void s2(OWLSubPropertyAxiom axiom) {
+    private void s2(OWLPropertyExpression subproperty, OWLPropertyExpression superproperty) {
 		//TODO implement
 	}
     
     //(n1)
     //already handled in OntologyProceeder
     
-    private void n2(OWLNaryPropertyAxiom axiom) {
+    private void n2(OWLPropertyExpression property1, OWLPropertyExpression property2) {
 		//TODO implement
     }
     
@@ -76,58 +73,51 @@ public class RuleCreatorQL extends RuleCreator {
     private void ir(OWLObjectProperty prop) {
 		//TODO implement
     }
-//*****************************************************************************
-    
+//*****************************************************************************    
 	/** Translate class subsumption axioms with general subclass (atomic or complex) and atomic superclass.
 	 * 
-	 * @param classExpression general subclass expression.
-	 * @param owlClass named superclass.
+	 * @param subclass general subclass expression.
+	 * @param superclass named superclass.
 	 **/
 	@Override
-	public void writeDoubledRules(OWLClassExpression classExpression,
-			OWLClassExpression owlClass) {
-		// TODO implement
+	public void writeDoubledRules(OWLClassExpression subclass,
+			OWLClassExpression superclass) {
+		s1(subclass, superclass);
 	}
 
 	/** Translate class subsumption axioms with atomic subclass and complex superclass, from equivalent axioms.
 	 * 
-	 * @param expression named subclass.
+	 * @param subclass named subclass.
 	 * @param superclass named superclass.
 	 **/
 	@Override
-	public void writeRuleC1(OWLClassExpression expression, OWLClass superclass,
+	public void writeRuleC1(OWLClassExpression subclass, OWLClass superclass,
 			boolean lastIndex) {
-		// TODO implement
+		s1(subclass, superclass);
 	}
 
 	/** Translate class subsumption axioms with atomic superclass and complex subclass (not atomic), from equivalent axioms.
 	 * 
-	 * @param owlClass named superclass.
-	 * @param rightPartOfRule complex (not atomic) subclass expression.
+	 * @param superclass named superclass.
+	 * @param subclass complex (not atomic) subclass expression.
 	 **/
 	@Override
-	public void writeEquivalentRule(OWLClass owlClass,
-			OWLClassExpression rightPartOfRule) {
-		// TODO implement
+	public void writeEquivalentRule(OWLClass superclass,
+			OWLClassExpression subclass) {
+		s1(subclass, superclass);
 	}
 
 	/** Translate property subsumption axioms with general subproperty (atomic or complex) and atomic superproperty
      *
-     * @param general (atomic or complex) subproperty expression
-	 * @param named superproperty
+     * @param subproperty general (atomic or complex) subproperty expression
+	 * @param superproperty named superproperty
 	 **/
 	@Override
-	public void writeRuleR1(OWLObjectPropertyExpression expression,
-			OWLObjectProperty superclass) {
-		// TODO implement
+	public void writeRuleR1(OWLObjectPropertyExpression subproperty,
+			OWLObjectProperty superproperty) {
+		s2(subproperty, superproperty);
 	}
 
-    /** Translates a given data property with its property name, individual, and value into a rule (or two rules).
-    *
-	*  @param data property.
-    *  @param individual.
-    *  @param value.
-    */
 	@Override
 	public void translateDataPropertyAssertion(OWLDataProperty dataProperty,
 			OWLIndividual individual, OWLLiteral value) {
